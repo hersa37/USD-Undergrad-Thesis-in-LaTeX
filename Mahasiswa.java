b4ad9c2204ab3d0b1fb9f538042c029ebec5b5ee
Bernardus Hersa <bernardus.hersa37@gmail.com>
2024-03-19 08:58:18 +0700

    Changed symlink to use use actual file for git purposes

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,1 +1,40 @@
-/home/echa/College/Semester 3/PBOL/Week03/PBOL_W03/src/main/java/penduduk/Mahasiswa.java
+package penduduk;
+
+public class Mahasiswa extends Penduduk implements Peserta{
+
+    private String nim;
+
+    public Mahasiswa() {}
+
+    public Mahasiswa(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    @Override
+    public double hitungIuran() {
+        return Double.parseDouble(nim) / 10000;
+    }
+
+    @Override
+    public String jenisSertifikat() {
+        return "Sertifikat Panitia";
+    }
+
+    @Override
+    public String getFasilitas() {
+        return "block note, alat tulis, laptop";
+    }
+
+    @Override
+    public String getKonsumsi() {
+        return "snack, makan siang, makan malam";
+    }
+}
